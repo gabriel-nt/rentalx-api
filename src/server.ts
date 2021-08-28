88d2b52a9aa4b4dcdd72226b649a92ed156daa78
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-08-28 18:23:38 -0300

    Initialized application

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
+
+import { router } from './routes/index.routes';
+
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3333, () => {
+  console.log('🎮 Server is running!');
+});
