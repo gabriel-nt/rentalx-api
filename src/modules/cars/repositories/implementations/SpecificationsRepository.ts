d7dfd427755195e6f1eb82c7d4bd71017a6d1d89
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-08-31 23:35:58 -0300

    Updated repositories and use cases

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../../model/Specification';
+import { Specification } from '../../entities/Specification';
 import {
   ISpecificationsRepository,
   ICreateSpecificationDTO,
@@ -25,7 +25,7 @@ class SpecificationsRepository implements ISpecificationsRepository {
 
   findByName(name: string): Specification {
     const specification = this.specifications.find(
-      (specification) => specification.name === name
+      (specification) => specification.name === name,
     );
 
     return specification;
