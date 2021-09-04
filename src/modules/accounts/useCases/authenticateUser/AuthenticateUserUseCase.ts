ccdf7bbefc2efab40aecea182830e861477b03ad
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-09-04 18:02:37 -0300

    Added exceptions treatment

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,6 +1,8 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
+
+import AppError from '../../../../errors/AppError';
 import IUsersRepository from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -26,13 +28,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Email or password incorrect');
+      throw new AppError('Email or password incorrect');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Email or password incorrect');
+      throw new AppError('Email or password incorrect');
     }
 
     const token = sign({}, '6c224e12f25f0ad981232a4503a49d0b', {
