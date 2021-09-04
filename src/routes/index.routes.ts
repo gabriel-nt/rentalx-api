80b1c535817fd33de10b33c9d641af18f9510faf
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-09-04 17:26:09 -0300

    Added user module

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { usersRoutes } from './users.routes';
 import { categoriesRoutes } from './categories.routes';
+import { autheticateRoutes } from './authenticate.routes';
 import { specificationsRouter } from './specifications.routes';
 
 const router = Router();
 
+router.use(autheticateRoutes);
 router.use('/users', usersRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRouter);
