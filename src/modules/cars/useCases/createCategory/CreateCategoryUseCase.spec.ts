fd5cf41a9344d2835f46b52c9b3fbff561d18289
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-09-07 10:04:53 -0300

    Added test for CreateCategoryUseCase

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,42 @@
-describe('Create category', () => {
-  it('Should be able to sum two values', () => {
-    const sum = 2 + 2;
-    const result = 5;
+import AppError from '../../../../errors/AppError';
+import CategoriesRepositoryInMemory from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-    expect(sum).toEqual(result);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    );
+  });
+
+  it('Should be able to create a new category', async () => {
+    const category = {
+      name: 'Category name',
+      description: 'Category description',
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name,
+    );
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a new category with name exists', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category name',
+        description: 'Category description',
+      };
+
+      await createCategoryUseCase.execute(category);
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
