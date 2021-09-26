d8f271b0fa29e49cf1f37e8e1e11e7dd63bdfabf
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-09-26 10:55:14 -0300

    Added sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,9 @@ import cors from 'cors';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
 import '@shared/container';
 import swaggerFile from '../../../swagger.json';
 import AppError from '@shared/errors/AppError';
@@ -25,10 +28,24 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
